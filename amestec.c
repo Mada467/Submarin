b5c8fed397b1047004b256523df5c3d31b9c8275
Mada467 <madalinalexandru467@gmail.com>
2026-01-04 19:49:56 +0200

    Am pus textura la hublou

--- amestec.c
+++ amestec.c
@@ -3,23 +3,49 @@
 #include <GL/glu.h>
 #include <GL/glaux.h>
 #include <math.h>
+#include <stdio.h>
 
 float posMisc = 0.0f;
 GLUquadricObj* pQuadric;
+GLuint texturi[1]; // Vectorul care retine ID-ul texturii
 
-// Pește compus din sferă (corp) și triunghi (coadă)
+// --- FUNCTIE INCARCARE TEXTURA ---
+void IncarcaTextura(const char* caleFisier, int indexTextura) {
+    BITMAP bmp;
+    // Incarcam imaginea de pe disc
+    HBITMAP hBmp = (HBITMAP)LoadImageA(NULL, caleFisier, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+
+    if (hBmp != NULL) {
+        GetObject(hBmp, sizeof(bmp), &bmp);
+        glGenTextures(1, &texturi[indexTextura]);
+        glBindTexture(GL_TEXTURE_2D, texturi[indexTextura]);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+        // Repetam textura pentru a acoperi toata rama
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, bmp.bmWidth, bmp.bmHeight,
+            0, GL_BGR_EXT, GL_UNSIGNED_BYTE, bmp.bmBits);
+        DeleteObject(hBmp);
+    }
+}
+
+// --- DESENARE PESTE (Corp: Sfera, Coada: Triunghi) ---
 void deseneazaPeste(float x, float y, float r, float g, float b, float viteza) {
     glPushMatrix();
-    float xAnimat = fmod(x + posMisc * viteza, 1.4f) - 0.2f; // Mișcare continuă
+    float xAnimat = fmod(x + posMisc * viteza, 1.4f) - 0.2f;
     glTranslatef(xAnimat, y + sin(posMisc * 4) * 0.03f, 0.0f);
 
     glColor3f(r, g, b);
-    glPushMatrix(); // Corpul
+    glPushMatrix(); // Corpul pestei
     glScalef(1.5f, 0.7f, 1.0f);
     gluSphere(pQuadric, 0.045, 20, 20);
     glPopMatrix();
 
-    glBegin(GL_TRIANGLES); // Coada
+    glBegin(GL_TRIANGLES); // Coada pestei
     glVertex2f(-0.04f, 0.0f);
     glVertex2f(-0.09f, 0.04f);
     glVertex2f(-0.09f, -0.04f);
@@ -31,40 +57,40 @@ void CALLBACK display(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     glLoadIdentity();
 
-    // 1. DEFINIM ZONA DIN INTERIORUL HUBLOULUI ÎN STENCIL
+    // 1. STENCIL: Masca pentru "gaura" hubloului
     glEnable(GL_STENCIL_TEST);
     glStencilFunc(GL_ALWAYS, 1, 0xFF);
     glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE);
 
-    glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE); // Nu desenăm culori încă
+    glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
     glPushMatrix();
     glTranslatef(0.5f, 0.5f, 0.0f);
-    gluDisk(pQuadric, 0, 0.35, 64, 1); // Aceasta este gaura
+    gluDisk(pQuadric, 0, 0.35, 64, 1);
     glPopMatrix();
     glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
 
-    // 2. DESENĂM SCENA MARINĂ (DOAR în interiorul cercului)
+    // 2. SCENA MARINA (Se vede doar prin gaura)
     glStencilFunc(GL_EQUAL, 1, 0xFF);
     glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
 
-    // Fundal Apă
+    // Fundal Apa
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glColor4f(0.0f, 0.2f, 0.4f, 1.0f);
     glRectf(0, 0, 1, 1);
 
-    // Nisip în mișcare
+    // Nisip miscator
     glPushMatrix();
     float nisipShift = fmod(posMisc * 0.05f, 0.4f);
     glTranslatef(-nisipShift, 0, 0);
     glColor3f(0.7f, 0.6f, 0.4f);
-    glRectf(-0.5, 0.0, 1.5, 0.25); // Nisipul nu va ieși din cerc datorită Stencil
+    glRectf(-0.5, 0.0, 1.5, 0.25);
     glPopMatrix();
 
-    // Cei 3 pești
-    deseneazaPeste(0.1, 0.6, 1.0, 0.5, 0.0, 0.3); // Portocaliu
-    deseneazaPeste(0.4, 0.4, 1.0, 0.1, 0.1, 0.5); // Roșu
-    deseneazaPeste(0.7, 0.3, 0.9, 0.9, 0.1, 0.2); // Galben
+    // Pestii
+    deseneazaPeste(0.1, 0.6, 1.0, 0.5, 0.0, 0.3);
+    deseneazaPeste(0.4, 0.4, 1.0, 0.1, 0.1, 0.5);
+    deseneazaPeste(0.7, 0.3, 0.9, 0.9, 0.1, 0.2);
 
     // Bule
     glColor4f(1.0f, 1.0f, 1.0f, 0.4f);
@@ -76,28 +102,42 @@ void CALLBACK display(void) {
         glPopMatrix();
     }
 
-    // 3. DESENĂM INTERIORUL SUBMARINULUI (DOAR în exteriorul cercului)
+    // 3. INTERIOR SUBMARIN (Peretele negru din jurul hubloului)
     glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
-    glColor3f(0.05f, 0.05f, 0.07f); // Negru/Gri închis (pereții submarinului)
+    glDisable(GL_TEXTURE_2D);
+    glColor3f(0.05f, 0.05f, 0.07f);
     glRectf(0, 0, 1, 1);
 
-    // 4. DESENĂM RAMA DE LEMN (Peste tot)
+    // 4. RAMA CU TEXTURA DE LEMN (Peste tot)
     glDisable(GL_STENCIL_TEST);
-    glColor3f(0.35f, 0.18f, 0.05f); // Maro lemn
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texturi[0]);
+    glColor3f(1.0f, 1.0f, 1.0f); // Alb pentru a nu colora textura
+
+    // Setam generarea automata a coordonatelor pentru disk
+    glEnable(GL_TEXTURE_GEN_S);
+    glEnable(GL_TEXTURE_GEN_T);
+    glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+    glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+
     glPushMatrix();
     glTranslatef(0.5f, 0.5f, 0.0f);
-    gluDisk(pQuadric, 0.35, 0.46, 64, 1); // Rama propriu-zisă
+    gluDisk(pQuadric, 0.35, 0.46, 64, 1);
+    glPopMatrix();
 
-    // Nituri pe ramă
+    glDisable(GL_TEXTURE_GEN_S);
+    glDisable(GL_TEXTURE_GEN_T);
+    glDisable(GL_TEXTURE_2D);
+
+    // 5. NITURI METALICE (Negre)
     glColor3f(0.1f, 0.1f, 0.1f);
     for (int i = 0; i < 360; i += 45) {
         float rad = i * 3.1415f / 180.0f;
         glPushMatrix();
-        glTranslatef(cos(rad) * 0.41f, sin(rad) * 0.41f, 0);
+        glTranslatef(0.5f + cos(rad) * 0.41f, 0.5f + sin(rad) * 0.41f, 0);
         gluDisk(pQuadric, 0, 0.015, 12, 1);
         glPopMatrix();
     }
-    glPopMatrix();
 
     auxSwapBuffers();
 }
@@ -108,8 +148,13 @@ void CALLBACK miscare(void) {
 }
 
 void myinit(void) {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
     pQuadric = gluNewQuadric();
     gluQuadricDrawStyle(pQuadric, GLU_FILL);
+
+    // --- INCARCARE TEXTURA ---
+    // Aici poti pune numele fisierului tau .bmp
+    IncarcaTextura("hublou.bmp", 0);
 }
 
 void CALLBACK myReshape(GLsizei w, GLsizei h) {
@@ -124,7 +169,7 @@ void CALLBACK myReshape(GLsizei w, GLsizei h) {
 int main(int argc, char** argv) {
     auxInitDisplayMode(AUX_DOUBLE | AUX_RGB | AUX_STENCIL);
     auxInitPosition(0, 0, 600, 600);
-    auxInitWindow("Submarin - Vedere Finala");
+    auxInitWindow("Submarin - Final cu Textura");
     myinit();
     auxReshapeFunc(myReshape);
     auxIdleFunc(miscare);
